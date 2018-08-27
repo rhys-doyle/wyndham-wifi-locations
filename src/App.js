962218ca2a0a4ec9c5222f58c0ce5774df6190ac
Rhys Doyle <rhys.doyle@gmail.com>
2018-08-27 15:49:18 +1000

    app.js updates

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "reset-css/reset.css";
+import "./App.css";
+import SideBar from "./Components/sidebar";
+import Map from "./Components/map";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="mainContainer">
+        <SideBar />
+        <Map />
       </div>
     );
   }
