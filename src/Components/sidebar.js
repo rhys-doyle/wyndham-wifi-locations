bc2b8c1dd1c1bbcb39ad1dff49917553bed54e63
Rhys Doyle <rhys.doyle@gmail.com>
2018-08-27 15:46:10 +1000

    create sidebar element

---
+++ src/Components/sidebar.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./sidebar.css";
+
+const SideBar = props => {
+  return (
+    <div className="sideBar">
+      <h1 className="title">Wyndham City Coucil Wi-Fi Locations</h1>
+      <div className="wyndhamLocations">test</div>
+    </div>
+  );
+};
