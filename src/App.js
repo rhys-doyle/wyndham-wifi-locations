89345b8f17cf0b394ad312b330c376d627a2eb1d
Rhys Doyle <rhys.doyle@gmail.com>
2018-08-28 17:47:17 +1000

    removed sidebar from app and put into map.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from "react";
 import "reset-css/reset.css";
 import "./App.css";
-import SideBar from "./Components/sidebar";
 import Map from "./Components/map";
 
 class App extends Component {
   render() {
     return (
       <div className="mainContainer">
-        <SideBar />
         <Map />
       </div>
     );
