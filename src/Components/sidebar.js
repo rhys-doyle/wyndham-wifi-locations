d4bbd5e626cd4316b0bd1a48bc75978fc4b4c830
Rhys Doyle <rhys.doyle@gmail.com>
2018-08-28 17:45:06 +1000

    props and map function for achor elements

--- src/Components/sidebar.js
+++ src/Components/sidebar.js
@@ -1,16 +1,23 @@
 import React from "react";
 import "./sidebar.css";
+import WifiLocation from "./wifiLoaction";
 
-const SideBar = props => {
+const Sidebar = props => {
   return (
     <div className="sideBar">
       <h1 className="title">Wyndham City Coucil Wi-Fi Locations</h1>
-      <div className="wyndhamLocations">test</div>
+      <div className="wyndhamLocations">
+        {props.wifiPoints.map((hotspot, index) => {
+          return (
+            <WifiLocation key={hotspot.id} index={index} name={hotspot.name} />
+          );
+        })}
+      </div>
     </div>
   );
 };
 
-export default SideBar;
+export default Sidebar;
 {
   /* <a
 href=""
