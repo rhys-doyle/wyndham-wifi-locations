06b6c519d7f48cde2d7d0770638c9e81f7c94a2d
Rhys Doyle <rhys.doyle@gmail.com>
2018-08-30 10:03:37 +1000

    add onclick to wifiLocation

--- src/Components/wifiLoaction.js
+++ src/Components/wifiLoaction.js
@@ -2,7 +2,14 @@ import React from "react";
 
 const WifiLocation = props => {
   return (
-    <a href="" className="hotspot">
+    <a
+      href=""
+      className="hotspot"
+      onClick={e => {
+        e.preventDefault();
+        props.onClick(props.id);
+      }}
+    >
       {props.name}
     </a>
   );
